da9f4a313f9555b6363d320ee5ea97a7d2a96166
happy531 <jedrzejwesolowski177@gmail.com>
2022-01-23 11:56:11 +0100

    add CrossOrigin to fix cors

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -3,8 +3,10 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:3000")
 public class BackendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
