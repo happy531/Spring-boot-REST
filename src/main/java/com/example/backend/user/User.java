4d3db347e4df3fd1fc516a9d9fca11003ad2aa96
Jędrzej Wesołowski <71727233+happy531@users.noreply.github.com>
2022-01-23 22:59:31 +0100

    Update User.java

--- src/main/java/com/example/backend/user/User.java
+++ src/main/java/com/example/backend/user/User.java
@@ -24,7 +24,7 @@ import java.util.Set;
 
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+   // @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
     private Long userId;
 
