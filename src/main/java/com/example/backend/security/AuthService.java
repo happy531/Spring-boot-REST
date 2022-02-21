3307fed5ad1c713dcea28aaa1dc12f9006dbd6f2
happy531 <jedrzejwesolowski177@gmail.com>
2022-02-21 14:29:24 +0100

    add AuthService

---
+++ src/main/java/com/example/backend/security/AuthService.java
@@ -0,0 +1,39 @@
+package com.example.backend.security;
+
+import com.example.backend.user.User;
+import com.example.backend.user.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    private void generateVerificationToken(User user) {
+        String verificationToken = UUID.randomUUID().toString();
+    }
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest){
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+        generateVerificationToken(user);
+    }
+
+}
+
